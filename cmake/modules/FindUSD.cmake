05a75414be76feeaea822cded3ef411577a65c10
Leonid Onokhov <sopvop@gmail.com>
2022-03-22 16:19:02 +0300

    [build] Build with conan

--- cmake/modules/FindUSD.cmake
+++ cmake/modules/FindUSD.cmake
@@ -1,86 +1,17 @@
-# Simple module to find USD.
+find_package(pxr REQUIRED)
 
-# Note that on Windows with USD <= 0.19.11, USD_LIB_PREFIX should be left at
-# default (or set to empty string), even if PXR_LIB_PREFIX was specified when
-# building core USD, due to a bug.
-
-# On all other platforms / versions, it should match the PXR_LIB_PREFIX used
-# for building USD (and shouldn't need to be touched if PXR_LIB_PREFIX was not
-# used / left at it's default value).
-
-set(USD_LIB_PREFIX ${CMAKE_SHARED_LIBRARY_PREFIX}
-    CACHE STRING "Prefix of USD libraries; generally matches the PXR_LIB_PREFIX used when building core USD")
-
-if (WIN32)
-    # ".lib" on Windows
-    set(USD_LIB_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX}
-        CACHE STRING "Extension of USD libraries")
-else ()
-    # ".so" on Linux, ".dylib" on MacOS
-    set(USD_LIB_SUFFIX ${CMAKE_SHARED_LIBRARY_SUFFIX}
-        CACHE STRING "Extension of USD libraries")
-endif ()
-
-# On a system with an existing USD /usr/local installation added to the system
-# PATH, use of PATHS in find_path incorrectly causes the existing USD
-# installation to be found.  As per
-# https://cmake.org/cmake/help/v3.4/command/find_path.html
-# and
-# https://cmake.org/pipermail/cmake/2010-October/040460.html
-# HINTS get searched before system paths, which produces the desired result.
 find_path(USD_INCLUDE_DIR
     NAMES
         pxr/pxr.h
     HINTS
         ${PXR_USD_LOCATION}
         $ENV{PXR_USD_LOCATION}
+        ${pxr_INCLUDE_DIR}
     PATH_SUFFIXES
         include
     DOC
         "USD Include directory"
 )
-
-find_library(USD_LIBRARY
-    NAMES
-        ${USD_LIB_PREFIX}usd${USD_LIB_SUFFIX}
-    HINTS
-        ${PXR_USD_LOCATION}
-        $ENV{PXR_USD_LOCATION}
-    PATH_SUFFIXES
-        lib
-    DOC
-        "Main USD library"
-)
-
-get_filename_component(USD_LIBRARY_DIR ${USD_LIBRARY} DIRECTORY)
-
-find_file(USD_GENSCHEMA
-    NAMES
-        usdGenSchema
-    PATHS
-        ${PXR_USD_LOCATION}
-        $ENV{PXR_USD_LOCATION}
-    PATH_SUFFIXES
-        bin
-    DOC
-        "USD Gen schema application"
-)
-
-find_file(USD_CONFIG_FILE
-    NAMES 
-        pxrConfig.cmake
-    PATHS 
-        ${PXR_USD_LOCATION}
-        $ENV{PXR_USD_LOCATION}
-    DOC "USD cmake configuration file"
-)
-
-# PXR_USD_LOCATION might have come in as an environment variable, and
-# it could also have been a hint-list, so we'll make sure we set it to
-# wherever we found pxrConfig, which is always the correct location.
-get_filename_component(PXR_USD_LOCATION "${USD_CONFIG_FILE}" DIRECTORY)
-
-include(${USD_CONFIG_FILE})
 
 if(DEFINED PXR_VERSION)
     # Starting in core USD 21.05, pxrConfig.cmake provides the various USD
@@ -129,11 +60,7 @@ include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(USD
     REQUIRED_VARS
-        PXR_USD_LOCATION
         USD_INCLUDE_DIR
-        USD_LIBRARY_DIR
-        USD_GENSCHEMA
-        USD_CONFIG_FILE
         USD_VERSION
         PXR_VERSION
     VERSION_VAR
